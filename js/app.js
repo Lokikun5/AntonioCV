126866a47df6e37de850ea972120113b9db06a1f
Quadjovie Antonio <quadjovie.antonio@gmail.com>
2022-01-02 19:46:55 +0100

    modif

--- js/app.js
+++ js/app.js
@@ -8,3 +8,15 @@ btnUp.addEventListener('click', () =>{
         behavior: "smooth"
     })
 })
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 120) {
+    nav.style.background = "pink";
+    nav.style.height ="50px";
+    }else {
+        nav.style.background = "white";
+        nav.style.height ="90px";
+    }
+  })
