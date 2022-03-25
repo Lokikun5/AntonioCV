ca12f435e052d789b315d431199f1b2ef3d5676e
Quadjovie Antonio <quadjovie.antonio@gmail.com>
2022-03-25 11:56:06 +0100

    modif

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
+        //  ----------------------DECLARATION
+
+
 const btnUp = document.querySelector('.btnUp');
+const nav = document.querySelector("nav");
+
+let date1 = new Date();
+
+//---------------------- parametre de date
+
+let dateLocale = date1.toLocaleString('fr-FR',{
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'});
+
+document.getElementById('p1').innerHTML = ' ' + dateLocale;
+
+p1.style.marginTop  = "20px";
+
+//----------------------- Bouton Up
 
 btnUp.addEventListener('click', () =>{
 
@@ -9,14 +32,17 @@ btnUp.addEventListener('click', () =>{
     })
 })
 
-const nav = document.querySelector("nav");
+//---------------------------- scroll nav
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 120) {
-    nav.style.background = "pink";
+    nav.style.background = "#2830AA";
     nav.style.height ="50px";
     }else {
         nav.style.background = "white";
         nav.style.height ="90px";
     }
   })
+
+  
+
